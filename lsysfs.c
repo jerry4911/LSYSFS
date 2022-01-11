6f8006c817444a59502188d72a521e04281d0f46
Lin Wen-Han <49276651+jerry4911@users.noreply.github.com>
2022-01-12 02:13:48 +0800

    Implement touch command
    1. 修改結構，存 st_atime、st_mtime
    - 因為 fuse版本問題，沒辦法直接用 stat()取得檔案資訊

--- lsysfs.c
+++ lsysfs.c
@@ -18,13 +18,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h> /* Definition of AT_* constants */
+#include <sys/stat.h>
 
 // ... //
+struct file{
 
-char dir_list[ 256 ][ 256 ];
+	char name[256];
+	time_t atime;
+	time_t mtime;
+	int parent_dir_index;
+
+};
+
+struct file dir_list[ 256 ];
+//char dir_list[ 256 ][ 256 ];
 int curr_dir_idx = -1;
 
-char files_list[ 256 ][ 256 ];
+struct file files_list[ 256 ];
+//char files_list[ 256 ][ 256 ];
 int curr_file_idx = -1;
 
 char files_content[ 256 ][ 256 ];
@@ -34,18 +46,21 @@ void add_dir( const char *dir_name )
 {
         printf("-----------------------\nadd_dir\n\n");
 	curr_dir_idx++;
-	strcpy( dir_list[ curr_dir_idx ], dir_name );
+	strcpy( dir_list[ curr_dir_idx ].name, dir_name );
+	dir_list[ curr_dir_idx ].atime = time( NULL );
+	dir_list[ curr_dir_idx ].mtime = time( NULL );
+
 }
 
 int is_dir( const char *path )
 {
-        printf("-----------------------\nis_dir\n\n");
+        
 	path++; // Eliminating "/" in the path
 
         int curr_idx;
 	
 	for ( curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
-		if ( strcmp( path, dir_list[ curr_idx ] ) == 0 )
+		if ( strcmp( path, dir_list[ curr_idx ].name ) == 0 )
 			return 1;
 	
 	return 0;
@@ -55,21 +70,24 @@ void add_file( const char *filename )
 {
         printf("-----------------------\nadd_file\n\n");
 	curr_file_idx++;
-	strcpy( files_list[ curr_file_idx ], filename );
+	strcpy( files_list[ curr_file_idx ].name, filename );
 	
 	curr_file_content_idx++;
 	strcpy( files_content[ curr_file_content_idx ], "" );
+
+	files_list[ curr_file_idx ].atime = time( NULL );
+	files_list[ curr_file_idx ].mtime = time( NULL );
 }
 
 int is_file( const char *path )
 {
-        printf("-----------------------\nis_file\n\n");
+
 	path++; // Eliminating "/" in the path
         
         int curr_idx;
 	
 	for ( curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
-		if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
+		if ( strcmp( path, files_list[ curr_idx ].name ) == 0 )
 			return 1;
 	
 	return 0;
@@ -82,7 +100,7 @@ int get_file_index( const char *path )
         int curr_idx;
 	
 	for ( curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
-		if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
+		if ( strcmp( path, files_list[ curr_idx ].name ) == 0 )
 			return curr_idx;
 	
 	return -1;
@@ -95,7 +113,7 @@ int get_dir_index( const char *path )
         int curr_idx;
 	
 	for ( curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
-		if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
+		if ( strcmp( path, files_list[ curr_idx ].name ) == 0 )
 			return curr_idx;
 	
 	return -1;
@@ -108,7 +126,9 @@ void write_to_file( const char *path, const char *new_content )
 	if ( file_idx == -1 ) // No such file
 		return;
 		
-	strcpy( files_content[ file_idx ], new_content ); 
+	strcpy( files_content[ file_idx ], new_content );
+	files_list[ file_idx ].atime = time( NULL );
+	files_list[ file_idx ].mtime = time( NULL );
 }
 
 // ... //
@@ -117,19 +137,40 @@ static int do_getattr( const char *path, struct stat *st )
 {
 	st->st_uid = getuid(); // The owner of the file/directory is the user who mounted the filesystem
 	st->st_gid = getgid(); // The group of the file/directory is the same as the group of the user who mounted the filesystem
-	st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
-	st->st_mtime = time( NULL ); // The last "m"odification of the file/directory is right now
+        if ( get_file_index( path ) == -1 ){
+
+		printf("New file!!!\n");
+		st->st_atime = time( NULL );
+		st->st_mtime = time( NULL ); 
+	}
+	else{
+		
+		printf("Old file......\n");
+
+	}
+	//st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
+	//st->st_mtime = time( NULL ); // The last "m"odification of the file/directory is right now
 	
 	if ( strcmp( path, "/" ) == 0 || is_dir( path ) == 1 )
 	{
 		st->st_mode = S_IFDIR | 0755;
 		st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
+		
+		int dir_index = get_dir_index( path );
+		st->st_atime = dir_list[ dir_index ].atime;
+		st->st_mtime = dir_list[ dir_index ].mtime;
 	}
 	else if ( is_file( path ) == 1 )
 	{
+		
 		st->st_mode = S_IFREG | 0644;
 		st->st_nlink = 1;
 		st->st_size = 1024;
+
+		int file_index = get_file_index( path );
+		st->st_atime = files_list[ file_index ].atime;
+		st->st_mtime = files_list[ file_index ].mtime;
+
 	}
 	else
 	{
@@ -142,10 +183,10 @@ static int do_getattr( const char *path, struct stat *st )
 static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
 {
     	printf("-----------------------\nread_dir\n\n");
-        printf("path is %s\n", path);
-        printf("offset is %lld\n", offset);
-        int path_index = get_file_index(path);
-        printf("path_index is %d\n", path_index);
+        printf("path is %s\n", path );
+        printf("offset is %lld\n", offset );
+        int path_index = get_dir_index( path );
+        printf("path_index is %d\n", path_index );
          
         filler( buffer, ".", NULL, 0 ); // Current Directory
 	filler( buffer, "..", NULL, 0 ); // Parent Directory
@@ -157,10 +198,10 @@ static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, o
                 int curr_idx;
 		
                 for ( curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
-			filler( buffer, dir_list[ curr_idx ], NULL, 0 );
+			filler( buffer, dir_list[ curr_idx ].name, NULL, 0 );
 	
 		for ( curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
-			filler( buffer, files_list[ curr_idx ], NULL, 0 );
+			filler( buffer, files_list[ curr_idx ].name, NULL, 0 );
 	}
 	
 	return 0; 
@@ -210,7 +251,7 @@ static int do_write( const char *path, const char *buffer, size_t size, off_t of
 	return size;
 }
 
-static int do_rmdir( const char *path)
+static int do_rmdir( const char *path )
 {
         printf("-----------------------\nrmdir\n\n");
 	int dir_index = get_dir_index( path );
@@ -218,13 +259,13 @@ static int do_rmdir( const char *path)
 	int curr_idx;
 		
         for ( curr_idx = dir_index+1; curr_idx < curr_dir_idx; curr_idx++ )
-		strcpy( dir_list[ curr_idx-1 ], dir_list[ curr_idx ]);
+		strcpy( dir_list[ curr_idx-1 ].name, dir_list[ curr_idx ].name);
 
 	curr_dir_idx--;
 	return 0;
 }
 
-static int do_unlink( const char *path)
+static int do_unlink( const char *path )
 {
         printf("-----------------------\nunlink\n\n");
         if ( is_file(path) ){
@@ -234,10 +275,41 @@ static int do_unlink( const char *path)
 		int curr_idx;
 			
 	        for ( curr_idx = file_index+1; curr_idx < curr_file_idx; curr_idx++ )
-			strcpy( dir_list[ curr_idx-1 ], dir_list[ curr_idx ]);
+			strcpy( files_list[ curr_idx-1 ].name, files_list[ curr_idx ].name);
 	
 		curr_file_idx--;
+
+		for ( curr_idx = file_index+1; curr_idx < curr_file_content_idx; curr_idx++ )
+			strcpy( files_content[ curr_idx-1 ], files_content[ curr_idx ]);
+	
+		curr_file_content_idx--;
 	}
+	return 0;
+}
+
+static int do_utimens( const char *path, const struct timespec ts[2])
+{
+        printf("-----------------------\nutimens\n\n");
+
+        if ( is_file(path) ){
+		
+		int file_idx = get_file_index( path );
+	
+		if ( file_idx == -1 ){
+			
+			path++;
+			add_file( path );
+		
+		}
+		else{
+
+			files_list[ file_idx ].atime = time( NULL );
+			files_list[ file_idx ].mtime = time( NULL );
+
+		}
+
+	}
+
 	return 0;
 }
 
@@ -250,6 +322,7 @@ static struct fuse_operations operations = {
     .write		= do_write,
     .rmdir              = do_rmdir,
     .unlink             = do_unlink,
+    .utimens            = do_utimens,
 };
 
 int main( int argc, char *argv[] )
